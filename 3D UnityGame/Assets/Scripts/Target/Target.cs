37939ecc65e5d2fcbeeab67a11f8d49534d21045
Jerome Marudo <114265897+JerGnome02@users.noreply.github.com>
2022-12-14 13:06:48 -0500

    Made large room and changed target script

--- 3D UnityGame/Assets/Scripts/Target/Target.cs
+++ 3D UnityGame/Assets/Scripts/Target/Target.cs
@@ -5,37 +5,53 @@ public class Target : MonoBehaviour
 {
     public float health = 10f;
     public bool isTargetPractice;
-    public float defaulthealth; 
+    public float defaulthealth;
     public float speed;
     public float lives;
-    public bool isAlive = true; 
+    public bool isAlive = true;
 
     public int targetsDestroyed = 0;
 
     public bool DoNotMove;
 
-    void Update() 
+
+    private float PosX;
+    private float PosY;
+    private float PosZ;
+
+    void Start()
     {
+        PosX = gameObject.transform.position.x;
+        PosY = gameObject.transform.position.y;
+        PosZ = gameObject.transform.position.z;
+    }
+
+    void Update()
+    {
+        
         if (DoNotMove == false)
         {
             moveTarget();
 
             keepInBounds();
         }
+        Debug.Log(PosX);
+        Debug.Log(PosY);
+        Debug.Log(PosZ);
     }
     public void TakeDamage(float amount)
     {
-        health -= amount; 
-        if(health <= 0f)
+        health -= amount;
+        if (health <= 0f)
         {
             Die();
-           // targetsDestroyed++;
+            // targetsDestroyed++;
             //if(targetsDestroyed == 10) {
-             //   if(gameObject.tag == "Door") {
-              //      gameObject.transform.LookAt(gameObject.transform.position + new Vector3(0,70,0));
-              //  }
-                
-           // }
+            //   if(gameObject.tag == "Door") {
+            //      gameObject.transform.LookAt(gameObject.transform.position + new Vector3(0,70,0));
+            //  }
+
+            // }
         }
 
     }
@@ -44,7 +60,7 @@ public class Target : MonoBehaviour
         if (isTargetPractice)
         {
             health = defaulthealth;
-            gameObject.transform.position = new Vector3(Random.Range(-50f,50f), Random.Range(0f, 25f), Random.Range(-50f,50f));
+            gameObject.transform.position = new Vector3(Random.Range((PosX - 33), (PosX + 33)), Random.Range(PosY, (PosY + 27)), Random.Range((PosZ - 33), (PosZ + 33)));
             KeepTrack(lives);
             lives--;
         }
@@ -52,44 +68,45 @@ public class Target : MonoBehaviour
         {
             Destroy(gameObject);
         }
-      
+
     }
 
     void moveTarget()
     {
-        
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         transform.Translate(Vector3.up * Time.deltaTime, Space.World);
-        
-        
+
+
     }
 
     void keepInBounds()
     {
-        if(gameObject.transform.position.x > 55 || gameObject.transform.position.x < -55 || gameObject.transform.position.z < -55 || gameObject.transform.position.z > 55)
+        if (gameObject.transform.position.x > (PosX + 38) || gameObject.transform.position.x < (PosX - 38) || gameObject.transform.position.z < (PosZ - 38) || gameObject.transform.position.z > (PosZ + 38) || gameObject.transform.position.y > (PosY + 27) || gameObject.transform.position.y < (PosY))
         {
-           gameObject.transform.position = new Vector3(Random.Range(-50f,50f), Random.Range(0f, 25f), Random.Range(-50f,50f));
-           gameObject.transform.LookAt(gameObject.transform.position + new Vector3(Random.Range(-50f,50f),0,0));
+            gameObject.transform.position = new Vector3(Random.Range((PosX - 33), (PosX + 33)), Random.Range(PosY, (PosY + 27)), Random.Range((PosZ - 33), (PosZ + 33)));
+            gameObject.transform.LookAt(new Vector3(Random.Range((PosX - 33), (PosX + 33)), Random.Range(PosY, (PosY + 27)), Random.Range((PosZ - 33), (PosZ + 33))));
         }
     }
     public bool KeepTrack(float lives)
     {
         Debug.Log(lives);
         Debug.Log("Much lvoe from Morjsioadjsioadsa");
-        if (lives <= 0){
+        if (lives <= 0)
+        {
             isAlive = false;
             this.gameObject.SetActive(false);
-            return isAlive; 
-           
-        } 
+            return isAlive;
+
+        }
         else
         {
             isAlive = true;
-            return isAlive; 
+            return isAlive;
         }
 
     }
 
 
-   
+
 }
